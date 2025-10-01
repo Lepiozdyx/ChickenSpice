0d0d13a322f9f14a158e511ba8b27980aec0c6ba
Developer <Lepiozdyx@gmail.com>
2025-10-01 08:51:50 -0300

    delete publish folder

--- ChickenSpice/App/AppDelegate.swift
+++ ChickenSpice/App/AppDelegate.swift
@@ -5,7 +5,6 @@ import UserNotifications
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
